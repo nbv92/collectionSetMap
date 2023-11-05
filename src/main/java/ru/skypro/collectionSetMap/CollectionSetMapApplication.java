024b69c1a71f86d78ee449cd33fb54230e118840
Timofey <timofey.sharipov@gmail.com>
2023-11-05 17:05:27 +0300

    first_commit

---
+++ src/main/java/ru/skypro/collectionSetMap/CollectionSetMapApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.collectionSetMap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionSetMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectionSetMapApplication.class, args);
+	}
+
+}
